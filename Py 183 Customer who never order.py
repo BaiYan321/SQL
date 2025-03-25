b8c2361611b02b46972f65c5917c3ebc31e7ae58
BaiYan321 <ryan.baiyan@gmail.com>
2025-03-25 13:59:15 -0700

    183 complete

---
+++ Py 183 Customer who never order.py
@@ -0,0 +1,15 @@
+# https://www.bilibili.com/video/BV1ZZ4y1c7YJ/?spm_id_from=333.337.search-card.all.click&vd_source=eae2c885960bda71fb3bd248c305cdbf
+
+import pandas as pd
+import numpy as np
+
+customers = pd.DataFrame({"id":[], \
+    "Name":["Joe", "Henry", "Sam", "Max"]})
+
+orders = pd.DataFrame({"Id": [1, 2] ,\
+    "CustomerId": [3, 1]})
+
+total = customers.merge(orders, how='Left', left_on="Id", right_on="CustomerId")
+
+total[total["CustomerId"].isna()]['Name'].values()
+# total[total["CustomerId"]==np.nan]['Name']
