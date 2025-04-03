0074f6093b63ecb96786019ef3b12d24d89b3879
BaiYan321 <ryan.baiyan@gmail.com>
2025-04-02 20:42:52 -0700

    video 3 start

--- SQL 534 Game Play Analysis 3.sql
+++ SQL 534 Game Play Analysis 3.sql
@@ -1,2 +1,5 @@
 -- https://www.bilibili.com/video/BV1KQ4y1k7km?spm_id_from=333.788.recommend_more_video.3&vd_source=eae2c885960bda71fb3bd248c305cdbf
 
+SELECT player_id, event_date,
+    SUM(game_played) OVER (PARTITION BY player_id ORDER BY event_date ASC) AS game_played_so_far
+FROM Activity
